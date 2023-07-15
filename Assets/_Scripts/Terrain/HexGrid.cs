dc03ef743dac19baf208fc1d882382abdf3dbe3b
Arno Poppe <minoarno@gmail.com>
2023-07-15 10:44:59 +0200

    ADD Hex Grid basics

--- Assets/_Scripts/Terrain/HexGrid.cs
+++ Assets/_Scripts/Terrain/HexGrid.cs
@@ -1,34 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HexGrid : MonoBehaviour
+namespace _Scripts.Terrain
 {
-    public int width = 6;
-    public int height = 6;
+    public class HexGrid : MonoBehaviour
+    {
+        public int width = 6;
+        public int height = 6;
 
-    public HexCell cellPrefab;
+        public HexCell cellPrefab;
     
-    HexCell[] cells;
+        HexCell[] _cells;
+        
+        public Text cellLabelPrefab;
 
-    void Awake () {
-        cells = new HexCell[height * width];
+        Canvas _gridCanvas;
+        HexMesh _hexMesh;
+        
+        void Awake () {
+            _gridCanvas = GetComponentInChildren<Canvas>();
+            _hexMesh = GetComponentInChildren<HexMesh>();
+            
+            _cells = new HexCell[height * width];
 
-        for (int z = 0, i = 0; z < height; z++) {
-            for (int x = 0; x < width; x++) {
-                CreateCell(x, z, i++);
+            for (int z = 0, i = 0; z < height; z++) {
+                for (int x = 0; x < width; x++) {
+                    CreateCell(x, z, i++);
+                }
             }
         }
-    }
 	
-    void CreateCell (int x, int z, int i) {
-        Vector3 position;
-        position.x = x * 10f;
-        position.y = 0f;
-        position.z = z * 10f;
+        void Start () {
+            _hexMesh.Triangulate(_cells);
+        }
+        
+        void CreateCell (int x, int z, int i) {
+            Vector3 position;
+            position.x = (x + z * 0.5f - z / 2) * (HexMetrics.InnerRadius * 2f);
+            position.y = 0f;
+            position.z = z * (HexMetrics.OuterRadius * 1.5f);
 
-        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
-        cell.transform.SetParent(transform, false);
-        cell.transform.localPosition = position;
+            HexCell cell = _cells[i] = Instantiate<HexCell>(cellPrefab);
+            cell.transform.SetParent(transform, false);
+            cell.transform.localPosition = position;
+            cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+            
+            Text label = Instantiate<Text>(cellLabelPrefab);
+            label.rectTransform.SetParent(_gridCanvas.transform, false);
+            label.rectTransform.anchoredPosition =
+                new Vector2(position.x, position.z);
+            label.text = cell.coordinates.ToStringOnSeparateLines();
+        }
     }
 }
--- Assets/_Scripts/Terrain/HexMetrics.cs
+++ Assets/_Scripts/Terrain/HexMetrics.cs
@@ -13,7 +13,39 @@ namespace _Scripts.Terrain
             new(InnerRadius, 0f, -0.5f * OuterRadius),
             new(0f, 0f, -OuterRadius),
             new(-InnerRadius, 0f, -0.5f * OuterRadius),
-            new(-InnerRadius, 0f, 0.5f * OuterRadius)
+            new(-InnerRadius, 0f, 0.5f * OuterRadius),
+            new Vector3(0f, 0f, OuterRadius)
         };
+    }
+    
+    [System.Serializable]
+    public struct HexCoordinates 
+    {
+        [SerializeField]
+        private int x, z;
+        public int X => x;
+
+        public int Z => z;
+
+        public int Y => -X - Z;
+
+        public HexCoordinates (int x, int z) 
+        {
+            this.x = x;
+            this.z = z;
+        }
+        
+        public static HexCoordinates FromOffsetCoordinates (int x, int z) {
+            return new HexCoordinates(x - z / 2, z);
+        }
+        
+        public override string ToString () {
+            return $"( {X}, {Y}, {Z} )";
+        }
+
+        public string ToStringOnSeparateLines ()
+        {
+            return $"{X}\n{Y}\n{Z}";
+        }
     }
 }
