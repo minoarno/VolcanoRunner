c975b99a706df0fcde32a71b4119e4f3ae40da28
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2023-07-15 11:19:30 +0200

    FIX resolved issue with raycast toggling all players

--- Assets/_Scripts/Networking/NetworkPlayer.cs
+++ Assets/_Scripts/Networking/NetworkPlayer.cs
@@ -7,21 +7,20 @@ public class NetworkPlayer : MonoBehaviour
 
     private static int _nrInstances = 0;
 
-    private int _playerId;
+    public int PlayerId;
 
     private Camera _mainCam;
     private Renderer _renderer;
 
     private Color _color;
 
-    private bool _isHit = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerId = _nrInstances++;
+        PlayerId = _nrInstances++;
 
-        int xPos = -5 + 5 * _playerId;
+        int xPos = -5 + 5 * PlayerId;
         transform.position = new Vector3(xPos, 0, 0);
 
         _mainCam = Camera.main;
@@ -29,7 +28,6 @@ public class NetworkPlayer : MonoBehaviour
 
 
         _color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        _renderer = GetComponentInChildren<Renderer>();
     }
 
     private void OnDestroy()
@@ -40,33 +38,25 @@ public class NetworkPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-        if (CheckRaycast()) _renderer.material.color = _color;
-        else _renderer.material.color = Color.white;
+        if (CheckRaycast()) GetComponent<Renderer>().material.color = _color;
+        else GetComponent<Renderer>().material.color = Color.white;
     }
 
     private bool CheckRaycast()
     {
-
-        _isHit = false;
-
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 10f;
-
-        mousePos = _mainCam.ScreenToWorldPoint(mousePos);
-
-
         if (!Input.GetMouseButton(0))
             return false;
 
-        Ray ray = _mainCam.ScreenPointToRay( Input.mousePosition);
-            //new Ray(_mainCam.transform.position, mousePos - _mainCam.transform.position);
+        Ray ray = _mainCam.ScreenPointToRay(Input.mousePosition);
 
         Debug.DrawRay(ray.origin, ray.direction * 100f, _color);
 
         RaycastHit hit;
 
-        return Physics.Raycast(ray, out hit, 100);
+        Physics.Raycast(ray, out hit, 100);
+
+        var hitPlayer = hit.collider.gameObject.GetComponent<NetworkPlayer>();
+
+        return hitPlayer?.PlayerId == PlayerId;
     }
 }
