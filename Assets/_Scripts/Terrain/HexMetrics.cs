343271551b905f3013b6317d88cd6b06cc863100
Arno Poppe <minoarno@gmail.com>
2023-10-23 23:21:57 +0200

    Perturbing

--- Assets/_Scripts/Terrain/HexMetrics.cs
+++ Assets/_Scripts/Terrain/HexMetrics.cs
@@ -8,10 +8,10 @@ namespace _Scripts.Terrain
         public const float OuterRadius = 10f;
         public const float InnerRadius = OuterRadius * 0.866025404f;
 
-        public const float SolidFactor = 0.75f;
+        public const float SolidFactor = 0.8f;
         public const float BlendFactor = 1f - SolidFactor;
 
-        public const float ElevationStep = 5f;
+        public const float ElevationStep = 3f;
 
         public const int TerracesPerSlope = 2;
         public const int TerraceSteps = TerracesPerSlope * 2 + 1;
@@ -19,7 +19,7 @@ namespace _Scripts.Terrain
         public const float VerticalTerraceStepSize = 1f / (TerracesPerSlope + 1);
 
         public static Texture2D noiseSource;
-        public const float cellPerturbStrength = 5f;
+        public const float cellPerturbStrength = 4f;
         public const float noiseScale = 0.003f;
         public const float elevationPerturbStrength = 1.5f;
 
@@ -153,6 +153,29 @@ namespace _Scripts.Terrain
             }
 
             return new HexCoordinates(iX, iZ);
+        }
+    }
+
+    public struct EdgeVertices
+    {
+        public Vector3 v1, v2, v3, v4;
+
+        public EdgeVertices(Vector3 corner1, Vector3 corner2)
+        {
+            v1 = corner1;
+            v2 = Vector3.Lerp(corner1, corner2, 1f / 3f);
+            v3 = Vector3.Lerp(corner1, corner2, 2f / 3f);
+            v4 = corner2;
+        }
+
+        public static EdgeVertices TerraceLerp(EdgeVertices a, EdgeVertices b, int step)
+        {
+            EdgeVertices result;
+            result.v1 = HexMetrics.TerraceLerp(a.v1, b.v1, step);
+            result.v2 = HexMetrics.TerraceLerp(a.v2, b.v2, step);
+            result.v3 = HexMetrics.TerraceLerp(a.v3, b.v3, step);
+            result.v4 = HexMetrics.TerraceLerp(a.v4, b.v4, step);
+            return result;
         }
     }
 }
